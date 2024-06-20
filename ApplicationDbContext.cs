0f3904e2e9f8efb6b2c31179a1891167f86b77a4
Werner Hansen | Tito <164363846+TitoHansen@users.noreply.github.com>
2024-06-20 12:32:09 -0400

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LabWebApp.Models
+{
+	public class Product
+	{
+        
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public string Description { get; set; }
+        
+
+    }
+}
+
